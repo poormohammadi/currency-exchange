c5c1e6963084fa7e2a490b234919b68d639db8a5
Ali Poormohammadi <poormohammmadi@gmail.com>
2021-11-14 16:33:51 +0400

    change api url to https

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosPromise } from 'axios';
 
 import { ExchangeRateResponse } from './../models/ExchangeRateResponse';
 
-const CURRENCY_EXCHANGE_RATE_URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=42a264e9d421d1e8b006e8537afca2d4&format=1';
+const CURRENCY_EXCHANGE_RATE_URL = 'https://api.exchangeratesapi.io/v1/latest?access_key=42a264e9d421d1e8b006e8537afca2d4&format=1';
 
 export function getExchangeRatesApi(): AxiosPromise<ExchangeRateResponse> {
     return axios.get(CURRENCY_EXCHANGE_RATE_URL);
